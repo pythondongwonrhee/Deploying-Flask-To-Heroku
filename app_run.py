5727d9ea1323a61203f8d0b72016e9abd3d1bb0c
pythondongwonrhee <47839780+pythondongwonrhee@users.noreply.github.com>
2019-03-04 16:08:38 -0500

    Update app_run.py

--- app_run.py
+++ app_run.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('login.html')
 @app.route('/index')
 def index():
     data = "Deploying a Flask App To Heroku"
